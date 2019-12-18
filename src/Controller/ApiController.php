da6c4e2f65dd8192c57df7f3625c444bd42555d4
Ridha Rahmi <ridha.rahmi13@gmail.com>
2019-12-18 16:29:49 +0100

    add files module

---
+++ src/Controller/ApiController.php
@@ -0,0 +1,56 @@
+<?php
+
+
+namespace Drupal\api_rest\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use GuzzleHttp\Exception\RequestException;
+
+class ApiController extends ControllerBase
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    public function titleItems()
+    {
+        return $this->t('List Item');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function items()
+    {
+        $client = \Drupal::httpClient();
+        $url = 'http://deploy:deploy_bitbucket_2017@investia-intra.elyosdigital.com/api/pme/passed';
+        //$items = [];
+        try {
+            $response = $client->get($url);
+            $data = $response->getBody();
+
+            $response = (object)json_decode($data, true);
+            foreach ($response->data as $dat) {
+                if(!$dat['logo']){
+                    $dat['logo'] =  file_create_url(drupal_get_path('module', 'api_rest') . '/assets/img/logo.png');
+                }
+                $items[] = [
+                    'id' => $dat['id'],
+                    'title' => $dat['raison_soc'],
+                    'logo' => $dat['logo']
+                ];
+            }
+        } catch (RequestException $e) {
+            watchdog_exception('api_rest', $e->getMessage());
+        }
+        //kint($items);
+        return [
+            '#theme' => 'list_items',
+            '#items' => $items,
+        ];
+
+    }
+
+
+}
+
